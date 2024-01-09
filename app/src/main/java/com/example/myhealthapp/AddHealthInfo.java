357b375e17523c4b06c93e7f9a3de16a8d139554
damates <kich_alex654@mail.ru>
2024-01-09 22:18:43 +0300

    Доработка ввода данных

--- app/src/main/java/com/example/myhealthapp/AddHealthInfo.java
+++ app/src/main/java/com/example/myhealthapp/AddHealthInfo.java
@@ -2,13 +2,86 @@ package com.example.myhealthapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddHealthInfo extends AppCompatActivity {
-
+    Calendar dateAndTime= Calendar.getInstance();
+    private TextView date;
+    private TextView time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_health_info);
+        date = findViewById(R.id.data);
+        time = findViewById(R.id.time);
+        Date currentDT = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("DD.MM.YY");
+        date.setText(df.format(currentDT));
+
+        SimpleDateFormat dft = new SimpleDateFormat("HH:mm");
+        time.setText(dft.format(currentDT.getTime()));
     }
+
+    // отображаем диалоговое окно для выбора даты
+    public void setDate(View v) {
+        new DatePickerDialog(AddHealthInfo.this, d,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH))
+                .show();
+    }
+
+    // отображаем диалоговое окно для выбора времени
+    public void setTime(View v) {
+        new TimePickerDialog(AddHealthInfo.this, t,
+                dateAndTime.get(Calendar.HOUR_OF_DAY),
+                dateAndTime.get(Calendar.MINUTE), true)
+                .show();
+    }
+    // установка начальных даты и времени
+    private void setInitialDate() {
+        SimpleDateFormat df = new SimpleDateFormat("DD.MM.YY");
+        date.setText(df.format(dateAndTime.getTimeInMillis()));
+        /*date.setText(DateUtils.formatDateTime(this,
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_NUMERIC_DATE | DateUtils.FORMAT_SHOW_YEAR
+                        ));*/
+
+    }
+    private void setInitialTime() {
+        SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+        time.setText(df.format(dateAndTime.getTimeInMillis()));
+        /*time.setText(DateUtils.formatDateTime(this,
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_TIME |DateUtils.FORMAT_ABBREV_TIME));*/
+    }
+    // установка обработчика выбора времени
+    TimePickerDialog.OnTimeSetListener t=new TimePickerDialog.OnTimeSetListener() {
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            dateAndTime.set(Calendar.MINUTE, minute);
+            setInitialTime();
+        }
+    };
+
+    // установка обработчика выбора даты
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateAndTime.set(Calendar.YEAR, year);
+            dateAndTime.set(Calendar.MONTH, monthOfYear);
+            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDate();
+        }
+    };
 }
