244ff6786b1b70fa5d4020ec0a69a762023ed47b
damates <kich_alex654@mail.ru>
2024-05-25 20:22:41 +0300

    исправлен формат даты

--- app/src/main/java/com/example/myhealthapp/utils/DateTransform.java
+++ app/src/main/java/com/example/myhealthapp/utils/DateTransform.java
@@ -8,9 +8,9 @@ import java.util.Date;
 
 public class DateTransform {
 
-    public static final  SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("DD.MM.YY");
+    public static final  SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.YY");
     public static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm");
-    public static final SimpleDateFormat DATE_TIME = new SimpleDateFormat("DD.MM.YY HH:mm");
-    public static final SimpleDateFormat UNIX_DATE_TIME = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
+    public static final SimpleDateFormat DATE_TIME = new SimpleDateFormat("dd.MM.YY HH:mm");
+    public static final SimpleDateFormat UNIX_DATE_TIME = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
 
 }
